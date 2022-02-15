9ec416a8cb7fc5bc081fcf28c562ac1528a20c49
olliiiver <o_kirst@OliverKs-MacBook-Pro.local>
2022-02-15 23:12:45 +0100

    serial example

---
+++ examples/arduino_serial/README.md
@@ -0,0 +1,5 @@
+# Arduino Serial
+
+Reads data from Arduino Pin 8.
+
+Outputs some additional information if `SML_DEBUG` is enabled (see platformio.ini).
---
+++ examples/arduino_serial/main.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "sml.h"
+#include <Arduino.h>
+#include <AltSoftSerial.h>
+
+// Reads data from Arduino Pin 8 (only this pin is possible, see AltSoftSerial)
+// SoftwareSerial will be too slow to read data even on 9600 baud. 
+
+AltSoftSerial inputSerial; 
+
+sml_states_t currentState;
+
+void readByte (unsigned char currentChar) {
+  currentState = smlState(currentChar);
+  if (currentState == SML_UNEXPECTED) {
+    Serial.print(F(">>> Unexpected byte\n"));
+  }
+  if (currentState == SML_FINAL) {
+    Serial.print(F(">>> Successfully received a complete message!\n"));
+  }
+  if (currentState == SML_CHECKSUM_ERROR) {
+    Serial.print(F(">>> Checksum error.\n"));
+  }
+}
+
+void setup () {
+  inputSerial.begin(9600);
+  Serial.begin(115200);
+  Serial.println(F("Starting"));
+}
+
+void loop () {
+  while (inputSerial.available() > 0) {
+    readByte(inputSerial.read());
+  }
+}
